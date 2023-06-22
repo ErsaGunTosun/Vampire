1fa4e54f6cd79062839eeed707da8a4cc0d97757
ErsaGun Tosun <fatihetosun+git@gmail.com>
2023-06-22 18:05:35 +0300

    used slahs command builder

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
 import { config } from 'dotenv';
-import { Client, GatewayIntentBits,Routes } from 'discord.js';
+import { Client, GatewayIntentBits, Routes } from 'discord.js';
+import { SlashCommandBuilder } from "@discordjs/builders"
 import { REST } from '@discordjs/rest';
 
 config();
 const TOKEN = process.env.TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
 
-const commands = [
-    {
-        name: 'nerede',
-        description: 'Ünlü bir yenilen adamın size birisini bulmaya yardım eder.',
-        options: [{
-            name: 'kisi',
-            description: "Bulunacak kişi",
-            type: 3,
-            required: true,
-        }]
-    },
-    {
-        name: 'nerdesin',
-        description: 'Ünlü bir yenilen adamın sözlerini tekrarlar.',
-    },
-];
+
 
 const client = new Client({
     intents: [
@@ -44,13 +30,21 @@ client.on('interactionCreate', async (interaction) => {
         if (interaction.commandName === 'nerede') {
             console.log(interaction.options.get('kisi').value);
             await interaction.reply({ content: `${interaction.options.get('kisi').value} burada be burada,Sende buradasın` });
-        }else if(interaction.commandName === 'nerdesin'){
+        } else if (interaction.commandName === 'nerdesin') {
             await interaction.reply({ content: `Buradayım be burada,Sende buradasın.` });
         }
     }
 });
 
 const main = async () => {
+
+    const WhereCommand = new SlashCommandBuilder()
+        .setName('nerede')
+        .setDescription('Ünlü bir yenilen adamın size birisini bulmaya yardım eder.')
+        .addStringOption(option => option.setName('kisi').setDescription('Bulunacak kişi').setRequired(true));
+    const commands = [
+        WhereCommand.toJSON()
+    ];
     try {
         console.log('Started refreshing application (/) commands.');
         await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
